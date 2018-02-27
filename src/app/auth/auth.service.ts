099bd7bd49cdee64bf42315d62dcdfa81ab34814
davidbasto <davidbasto@gmail.com>
2018-02-26 21:28:57 -0300

    Adicionando guard para evitar acesso pela URL. Criando o training Service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+
+import { User } from './user.model';
+import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs/Subject';
+import { Router } from '@angular/router';
+
+
+@Injectable()
+export class AuthService {
+
+  private user: User;
+  authChange = new Subject<boolean>();
+
+  constructor(private router: Router) { }
+
+  registerUser(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authSuccessfully();
+  }
+
+  login(authData: AuthData) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authSuccessfully();
+  }
+
+  logout() {
+    this.user = null;
+    this.authChange.next(false);
+    this.router.navigate(['/login']);
+  }
+
+  isAuth() {
+    return this.user != null;
+  }
+
+  getUser() {
+    return { ...this.user };
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
+  }
+
+}
--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
+import { TrainingService } from '../training.service';
+
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
@@ -8,10 +10,12 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class NewTrainingComponent implements OnInit {
 
   @Output() trainingStart = new EventEmitter<void>();
+  availableExercises;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.availableExercises = this.trainingService.avaliableExercises;
   }
 
   onStartTraining() {
